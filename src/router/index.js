e120bf4116e2975cb3e570ba8d9db8ee9ad22618
rediske <854706195@qq.com>
2022-07-06 01:20:51 +0800

    嵌套路由的实现

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue';
+import VueRouter from "vue-router";
+//导入视图组件
+import Main from "../views/Main";
+import Login from "../views/Login";
+//导入子模块
+import UserList from "../views/user/List";
+import UserProfile from "../views/user/Profile";
+
+//使用
+Vue.use(VueRouter);
+//导出
+export default new VueRouter({
+  routes: [
+    {
+      //首页
+      path: '/main',
+      component: Main,
+      //写入子模块
+      children: [
+        {
+          path: '/user/profile',
+          component: UserProfile
+        },
+        {
+          path: '/user/list',
+          component: UserList
+        }
+      ]
+    },
+    {
+      //登录页
+      path: '/login',
+      component: Login
+    }
+  ]
+})
